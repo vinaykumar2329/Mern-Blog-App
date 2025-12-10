4c8d24bba2e444146c85fec3e06481c277e1a090
vinay <vinaykumar2329@gmail.com>
2025-12-10 22:51:08 +0530

    mobile view updated

--- Blog-App/src/pages/Updateblog.jsx
+++ Blog-App/src/pages/Updateblog.jsx
@@ -48,19 +48,20 @@ const Updateblog = () => {
         }
     }
   return (
-    <div className='text-center  flex flex-col h-[90vh] justify-center items-center gap-3 bg-[url("https://plus.unsplash.com/premium_photo-1674834298045-e405bc99076b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y2xvdWR8ZW58MHx8MHx8fDA%3D")] bg-cover bg-center'>
-      <div className='border-4 border-white p-5 py-5 flex flex-col gap-3 rounded-md backdrop-blur-xs'>
+    <div className='text-center min-h-screen w-full sm:p-5  flex flex-col h-[90vh] justify-center items-center gap-3 bg-[url("https://plus.unsplash.com/premium_photo-1674834298045-e405bc99076b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y2xvdWR8ZW58MHx8MHx8fDA%3D")] bg-cover bg-center'>
+      <div className='w-[90%] max-w-xs border-4 border-white p-5 py-5 flex flex-col gap-3 rounded-md backdrop-blur-xs'>
+
       <h1 className='text-2xl mb-3'>UPDATE POST</h1>
-      <form  className='flex flex-col gap-2 w-100 text-start' onSubmit={handleSubmit}>
+      <form  className='flex flex-col gap-2 w-full text-start' onSubmit={handleSubmit}>
 
         <label htmlFor="username" className='font-bold'>Username</label>
-        <input className='border-gray-500 border-2 px-3 py-2 rounded-2xl'  type="text" name="title" value={inputs.title} onChange={handleChange} />
+        <input className='border-gray-500 border-2 px-3 py-2 rounded-2xl w-full'  type="text" name="title" value={inputs.title} onChange={handleChange} />
 
         <label htmlFor="email" className='font-bold'>Email</label>
-        <input className='border-gray-500 border-2 px-3 py-2 rounded-2xl' type="text" name="content" value={inputs.content} onChange={handleChange} />
+        <input className='border-gray-500 border-2 px-3 py-2 rounded-2xl w-full' type="text" name="content" value={inputs.content} onChange={handleChange} />
 
         <label htmlFor="password" className='font-bold'>Password</label>
-        <input className='border-gray-500 border-2 px-3 py-2 rounded-2xl' type='url' name="image" value={inputs.image} onChange={handleChange} />
+        <input className='border-gray-500 border-2 px-3 py-2 rounded-2xl w-full' type='url' name="image" value={inputs.image} onChange={handleChange} />
 
         <div className='flex justify-center'>
         <button className='bg-emerald-400 p-2 w-50 text-amber-50' type='submit'>POST</button>
